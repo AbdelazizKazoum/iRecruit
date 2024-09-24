81713625280db8cd2e2cd16ac175593c5cb51ec7
Abdelaziz_kazoum <abdukazoum@gmail.com>
2024-09-24 15:22:06 +0100

    create docker compose file

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! my name is abdelaziz kazoum and this app runs inside a docker container ! ';
+    return 'Hello my friend ! my name is abdelaziz kazoum and this app runs inside a docker container ! ';
   }
 }

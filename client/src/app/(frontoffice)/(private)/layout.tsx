762072740a5f7dacd8b30f97673660a7c97ced72
Abdelaziz kazoum <abdukazoum@gmail.com>
2024-11-11 02:44:45 +0100

    add bubbles desing and watch vedio tutuial

--- client/src/app/(frontoffice)/(private)/layout.tsx
+++ client/src/app/(frontoffice)/(private)/layout.tsx
@@ -3,8 +3,6 @@ import AuthGuard from "@/hocs/AuthGuard";
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-
-    
   return (
     <Providers>
       <AuthGuard>{children}</AuthGuard>

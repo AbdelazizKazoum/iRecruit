fff3c665ea3150be7df9ad76b297435cabbe8e6a
Abdelaziz kazoum <abdukazoum@gmail.com>
2024-11-26 22:35:26 +0100

    redesign reset form & update password

--- client/src/app/(blank-pages)/(guest-only)/register/password/[code]/page.tsx
+++ client/src/app/(blank-pages)/(guest-only)/register/password/[code]/page.tsx
@@ -18,6 +18,28 @@ const page = async ({ params }: { params: { code: string } }) => {
   }
   return (
     <div className="flex flex-col items-center justify-center bg-primary-300/5 min-h-screen">
+      <a
+        className=" absolute top-2 left-10 mt-10 w-fit text-black-600/80 dark:text-white"
+        href="/home"
+      >
+        <div className="flex w-fit items-center lg:pl-0 lg:pt-0 xl:pt-0">
+          <svg
+            stroke="currentColor"
+            fill="currentColor"
+            strokeWidth="0"
+            viewBox="0 0 320 512"
+            className="mr-3 h-[13px] w-[8px] text-zinc-950 dark:text-white"
+            height="1em"
+            width="1em"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z" />
+          </svg>
+          <p className="ml-0 text-sm text-zinc-950 dark:text-white">
+            Retour au site web
+          </p>
+        </div>
+      </a>
       {/* Logo above the form */}
       <div className="h-20 w-60 relative block  mb-8">
         <Image

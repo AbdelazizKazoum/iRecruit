fe8e981a5b80a14dc2f2a594b52470b2b6270f68
Abdelaziz kazoum <abdukazoum@gmail.com>
2024-12-14 14:30:18 +0100

    creation de l'api pout la candidature

--- server/src/candidature/dto/update-candidature.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCandidatureDto } from './create-candidature.dto';
-
-export class UpdateCandidatureDto extends PartialType(CreateCandidatureDto) {}

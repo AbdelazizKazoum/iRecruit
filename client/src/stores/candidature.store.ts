3d18773015bbcf2bfce21439aa55e3942b154592
Abdelaziz_kazoum <abdukazoum@gmail.com>
2024-12-19 09:33:26 +0100

    create the store for candidature

--- client/src/stores/candidature.store.ts
+++ client/src/stores/candidature.store.ts
@@ -2,7 +2,7 @@ import { CandidatureType } from "@/types/candidature.types";
 import { create } from "zustand";
 
 export interface CandidatureStoreState {
-  candidatureData: CandidatureType;
+  candidatureData: CandidatureType | null;
   loading: boolean;
   error: string;
 
@@ -14,19 +14,107 @@ export interface CandidatureStoreState {
   ) => void;
 }
 
-export const useCandidatureStore = create((set) => ({
-  candidatureData: {},
+export const useCandidatureStore = create<CandidatureStoreState>((set) => ({
+  candidatureData: {
+    // Personal Information
+    personalInformation: {
+      prenom: "",
+      prenomAr: "",
+      nom: "",
+      nomAr: "",
+      adresse: "",
+      adresseAr: "",
+      lieuNaissance: "",
+      cin: "",
+      dateNaissance: new Date(),
+      sexe: "feminin", // default value, change as needed
+      situation: "celibataire", // default value, change as needed
+      telephone: "",
+      email: "",
+      experiences: {
+        fonctionnaire: false,
+        fonction: "",
+        ppr: "",
+        attestation: "",
+      },
+      situationDeHandicap: {
+        handicap: false,
+        typeHandicap: "",
+      },
+      files: null,
+    },
+
+    // Professional Information
+    professionalInformation: {
+      parcoursEtDiplomes: [
+        {
+          origine: "marocainPublic", // default value, change as needed
+          intituleDiplome: "",
+          diplomeType: "",
+          anneeObtention: 0,
+          specialite: "",
+          mention: "",
+          etablissement: "",
+          diplomePdf: "",
+        },
+      ],
+      niveauxLangues: [
+        {
+          langue: "",
+          niveau: "basique", // default value, change as needed
+          certificatLanguePdf: "",
+        },
+      ],
+      experiencePedagogique: {
+        experiencePedagogiqueEnHeures: 0,
+      },
+      publications: [
+        {
+          titre: "",
+          anneePublication: 0,
+          type: "",
+          url: "",
+          publicationPdf: "",
+        },
+      ],
+      communications: [
+        {
+          titre: "",
+          anneeCommunication: 0,
+          url: "",
+          communicationPdf: "",
+        },
+      ],
+      residanat: {
+        residanatPdf: "",
+      },
+    },
+  },
   loading: false,
   error: "",
 
-  setPersonalInformation : (data: CandidatureType["personalInformation"])=> {
-    try {
-        const res = 
-        
-    } catch (error) {
-        
-    }
+  setPersonalInformation: (data) => {
+    set((state) => ({
+      candidatureData: state.candidatureData
+        ? {
+            ...state.candidatureData,
+            personalInformation: data,
+          }
+        : null,
+    }));
+  },
 
-  }
-
+  setDiplomes: (data) => {
+    set((state) => ({
+      candidatureData: state.candidatureData
+        ? {
+            ...state.candidatureData,
+            professionalInformation: {
+              ...state.candidatureData.professionalInformation,
+              parcoursEtDiplomes: data,
+            },
+          }
+        : null,
+    }));
+  },
 }));

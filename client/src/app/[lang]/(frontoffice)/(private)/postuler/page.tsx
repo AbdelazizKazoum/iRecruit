1a12c1814519843d479f0467cd2c199a06d98878
Abdelaziz_kazoum <abdukazoum@gmail.com>
2024-12-25 12:07:46 +0100

    start

---
+++ client/src/app/[lang]/(frontoffice)/(private)/postuler/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="max-w-screen-2xl mt-24 pb-24 px-4 sm:px-8 xl:px-16 mx-auto">
+      page
+    </div>
+  );
+};
+
+export default page;

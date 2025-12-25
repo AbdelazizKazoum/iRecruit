d564ce6fe9df4a1e736ac3cd3acdbe79ee7737d1
Abdelaziz_kazoum <abdukazoum@gmail.com>
2025-12-25 14:23:05 +0100

    refactor: update AuthRedirect component to use router for navigation instead of redirect

--- client/src/components/AuthRedirect.tsx
+++ client/src/components/AuthRedirect.tsx
@@ -4,7 +4,9 @@ import { Locale } from "@/configs/i18n";
 import themeConfig from "@/configs/themeConfig";
 import { getLocalizedUrl } from "@/utils/i18n";
 // Next Imports
-import { redirect, usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
+import { useEffect } from "react";
+import Loading from "./Loading";
 
 // Type Imports
 // import type { Locale } from '@configs/i18n'
@@ -17,14 +19,20 @@ import { redirect, usePathname } from "next/navigation";
 
 const AuthRedirect = ({ lang }: { lang: Locale }) => {
   const pathname = usePathname();
+  const router = useRouter();
 
-  const redirectUrl = `/${lang}/login?redirectTo=${pathname}`;
-  const login = `/${lang}/login`;
-  const homePage = getLocalizedUrl(themeConfig.homePageUrl, lang);
+  useEffect(() => {
+    const redirectUrl = `/${lang}/login?redirectTo=${pathname}`;
+    const login = `/${lang}/login`;
+    const homePage = getLocalizedUrl(themeConfig.homePageUrl, lang);
 
-  return redirect(
-    pathname === login ? login : pathname === homePage ? login : redirectUrl
-  );
+    const target =
+      pathname === login ? login : pathname === homePage ? login : redirectUrl;
+
+    router.push(target);
+  }, [lang, pathname, router]);
+
+  return <Loading />;
 };
 
 export default AuthRedirect;
